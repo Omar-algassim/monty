5fca16464dcacf18256264d1edec28a62689e0c1
Omar-algassim <omarggg2@gmail.com>
2023-12-14 04:43:26 -0800

    update

--- pint.c
+++ pint.c
@@ -13,7 +13,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L %d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
 		free(info.op);
 		free_stack(*stack);
 		fclose(info.file);
